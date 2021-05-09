8d8a57f6751e22597d7ca3ff082b437890483aa0
Dizzy <57321204+519880746@users.noreply.github.com>
2021-05-09 17:12:04 +0800

    Add files via upload

---
+++ 1.1/HelloNumbers2.java
@@ -0,0 +1,10 @@
+public class HelloNumbers2{
+ public static void main (String[] args){
+    int x = 0;
+    while(x<10){
+        x= (x+1)*x/2;
+        System.out.print(x+"");
+        x++;
+        }
+    }
+}   
